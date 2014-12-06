e59cd1f33a244fdbd96f3358bd2c4385b55c9112
Myles Beardsmore <mediabeastnz@gmail.com>
2014-12-06 15:50:09 +1300

    updated url for production

--- public/js/game.js
+++ public/js/game.js
@@ -1,7 +1,7 @@
 $(document).foundation();
 
-//var base_url = 'http://pong.platocreative.co.nz';
-var base_url = 'http://ping-pong.app';
+var base_url = 'http://pong.platocreative.co.nz';
+//var base_url = 'http://ping-pong.app';
 
 var gameID = $("body").data("gameid");
 
