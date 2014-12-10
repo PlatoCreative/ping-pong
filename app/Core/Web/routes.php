b9200217782b10d06e77a10967001c6687617bd7
Michael Hopkins <michael@appitventures.com>
2014-12-10 05:11:31 -0700

    tidy up routes file a small bit

--- app/Core/Web/routes.php
+++ app/Core/Web/routes.php
@@ -7,24 +7,23 @@ Route::group(['namespace' => 'Web\Controllers'], function () {
     });
 
     // Dashboard
-    Route::get('dashboard', array('as' => 'dashboard', 'uses' => 'DashboardController@index'));
+    Route::get('dashboard', ['as' => 'dashboard', 'uses' => 'DashboardController@index']);
 
     // Start game
-    Route::post('game/create', array('as' => 'game/create', 'uses' => 'GameController@create'));
+    Route::post('game/create', ['as' => 'game/create', 'uses' => 'GameController@create']);
 
     // Game Route
-    Route::get('game/{game}', array('as' => 'game', 'uses' => 'GameController@index'));
+    Route::get('game/{game}', ['as' => 'game', 'uses' => 'GameController@index']);
 
     // Route for updating game scores
-    Route::post('game/{game}/score/{teamPos}', array('as' => 'game/score', 'uses' => 'GameController@updateGameScore'));
-    Route::get('game/{game}/streak/{teamPos}/{length}', array('as' => 'game/streak', 'uses' => 'GameController@addScoreStreak'));
+    Route::post('game/{game}/score/{teamPos}', ['as' => 'game/score', 'uses' => 'GameController@updateGameScore']);
+    Route::get('game/{game}/streak/{teamPos}/{length}', ['as' => 'game/streak', 'uses' => 'GameController@addScoreStreak']);
 
 
     // Mark game as completed/finished
-    Route::get('game/end/{game}', array('as' => 'game/end', 'uses' => 'GameController@end'));
+    Route::get('game/end/{game}', ['as' => 'game/end', 'uses' => 'GameController@end']);
 
 
-// Route for creating users
-    Route::get('players/create', array('as' => 'players/create', 'uses' => 'PlayerController@create'));
-    Route::post('players/store', array('as' => 'players/store', 'uses' => 'PlayerController@store'));
+    // Route for creating users
+    Route::resource('players','PlayersController',['only'=>['create','store']]);
 });
